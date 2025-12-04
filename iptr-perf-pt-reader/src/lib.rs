66cd6885aca460c462b38fbcc1f49bb119b85ebd
Evian-Zhang <evianzhang1999@163.com>
2025-12-04 21:00:29 +0800

    Use custom parser

--- iptr-perf-pt-reader/src/lib.rs
+++ iptr-perf-pt-reader/src/lib.rs
@@ -1,4 +1,7 @@
-use std::{borrow::Cow, io::Cursor};
+use std::{
+    borrow::Cow,
+    io::{Cursor, Read},
+};
 
 use byteorder::LittleEndian;
 use linux_perf_data::{
@@ -9,40 +12,50 @@ use crate::error::ReaderResult;
 
 pub mod error;
 
+const PERF_RECORD_AUXTRACE: u32 = 71;
+
 pub fn extract_pt_aux_data(perf_data: &[u8]) -> ReaderResult<Vec<&[u8]>> {
     let mut pt_aux_datas = vec![];
 
-    let PerfFileReader {
-        mut perf_file,
-        mut record_iter,
-    } = PerfFileReader::parse_file(Cursor::new(perf_data))?;
-    while let Some(record) = record_iter.next_record(&mut perf_file)? {
-        let PerfFileRecord::UserRecord(record) = record else {
-            continue;
+    let mut cursor = Cursor::new(perf_data);
+    let _ = PerfFileReader::parse_file(cursor.by_ref())?;
+    let mut pos = cursor.position() as usize; // Skip Perf Header
+
+    loop {
+        let Some(perf_event_header) = read_header(perf_data, &mut pos) else {
+            break;
         };
-        if !matches!(record.record_type, UserRecordType::PERF_AUXTRACE) {
-            continue;
+        match perf_event_header.r#type {
+            PERF_RECORD_AUXTRACE => {}
+            _ => {}
         }
-
-        let mut data = record.data;
-        let size = data.read_u64::<LittleEndian>()?;
-        let offset = data.read_u64::<LittleEndian>()?;
-        let _reference = data.read_u64::<LittleEndian>()?;
-        let idx = data.read_u32::<LittleEndian>()?;
-        let _tid = data.read_u32::<LittleEndian>()?;
-        let _cpu = data.read_u32::<LittleEndian>()?;
-        let _reserved = data.read_u32::<LittleEndian>()?;
-
-        println!(
-            "misc {:x} size {size:x} offset {offset:x} _reference {_reference:x} idx {idx:x} next {:x}",
-            record.misc,
-            data.as_slice().len()
-        );
-
-        // let Some(aux_data) = perf_data.get()
-
-        // pt_aux_datas.push(perf_data);
     }
 
     Ok(pt_aux_datas)
+}
+
+struct PerfEventHeader {
+    r#type: u32,
+    misc: u16,
+    size: u16,
+}
+
+fn read_header(perf_data: &[u8], pos: &mut usize) -> Option<PerfEventHeader> {
+    let bytes = perf_data
+        .get(*pos..)
+        .and_then(|buf| buf.first_chunk::<4>())?;
+    let r#type = u32::from_le_bytes(*bytes);
+    *pos += 4;
+    let bytes = perf_data
+        .get(*pos..)
+        .and_then(|buf| buf.first_chunk::<2>())?;
+    let misc = u16::from_le_bytes(*bytes);
+    *pos += 2;
+    let bytes = perf_data
+        .get(*pos..)
+        .and_then(|buf| buf.first_chunk::<2>())?;
+    let size = u16::from_le_bytes(*bytes);
+    *pos += 2;
+
+    Some(PerfEventHeader { r#type, misc, size })
 }
