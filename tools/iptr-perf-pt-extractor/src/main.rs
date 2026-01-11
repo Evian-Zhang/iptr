3977bb8caa6f70f082cbc62185f3efe4c9520904
Evian-Zhang <evianzhang1999@163.com>
2026-01-11 16:17:06 +0800

    Add option to only extract first PT trace from perf.data

--- tools/iptr-perf-pt-extractor/src/main.rs
+++ tools/iptr-perf-pt-extractor/src/main.rs
@@ -4,20 +4,34 @@ use anyhow::{Context, Result};
 use clap::Parser;
 
 /// Extract Intel PT aux data from perf.data
+///
+/// Set the environment variable `RUST_LOG=trace` for logging.
 #[derive(Parser)]
 struct Cmdline {
     /// Path of perf.data
     #[arg(short, long)]
     input: PathBuf,
-    /// Output directory
+    /// Path for output.
+    ///
+    /// If no `--first-only` is specified, this path should refer to
+    /// a directory, all PT traces inside the perf.data will be extracted
+    /// into that directory; if `--first-only` is specified, this option
+    /// is used for the file path for extracted PT trace.
     #[arg(short, long)]
-    output_dir: PathBuf,
+    output: PathBuf,
+    /// Only extract the first PT trace, ignoring all others.
+    #[arg(long)]
+    first_only: bool,
 }
 
 fn main() -> Result<()> {
     env_logger::init();
 
-    let Cmdline { input, output_dir } = Cmdline::parse();
+    let Cmdline {
+        input,
+        output,
+        first_only,
+    } = Cmdline::parse();
 
     let file = File::open(&input).context("Failed to open input file")?;
     // SAFETY: check the safety requirements of memmap2 documentation
@@ -27,7 +41,13 @@ fn main() -> Result<()> {
     let pt_auxtraces = iptr_perf_pt_reader::extract_pt_auxtraces(&buf)?;
 
     for pt_auxtrace in pt_auxtraces {
-        let target_path = output_dir.join(format!(
+        if first_only {
+            std::fs::write(&output, pt_auxtrace.auxtrace_data)
+                .context("Failed to write auxtrace data")?;
+            log::info!("Extracted {}", output.display());
+            return Ok(());
+        }
+        let target_path = output.join(format!(
             "{}-aux-idx{}.bin",
             origin_filename.display(),
             pt_auxtrace.idx
